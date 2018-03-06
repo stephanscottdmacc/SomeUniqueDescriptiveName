656e955caa6f45c6db7ba7a16b1a699fb7f1c45b
Stephan <sjscott1@dmacc.edu>
2018-03-05 19:20:55 -0600

    Committing to my branch.

---
+++ myFirstRepoStephanScott/SomeUniqueDescriptiveName-1/TheBrain.java
@@ -0,0 +1,10 @@
+//Lines of code and cool commands to take over the world
+
+public class TheBrain {
+
+	public static void main(String[] args) {
+		System.out.println("does it work?");
+		System.out.println("So far, so good...");
+		// add more
+	}
+}
